dd719141e6ea2f75acfcc3ad8242a197564320d4
healer512 <mingx862@gmail.com>
2022-01-27 23:02:54 +0800

    Thu Jan 27 23:02:46     2022-test

--- upload.sh
+++ upload.sh
@@ -5,13 +5,5 @@ read -p "Enter some information > " messae
 git add -A
 git commit -m "$time-$messae"
 git push
-if [ -n $BROWSER ]; then
-  $BROWSER 'https://github.com/healer512/my-image'
-elif which xdg-open > /dev/null; then
-  xdg-open 'https://github.com/healer512/my-image'
-elif which gnome-open > /dev/null; then
-  gnome-open 'https://github.com/healer512/my-image'
-# elif bla bla bla...
-else
-  echo "Could not detect the web browser to use."
-fi
+python -mwebbrowser https://github.com/healer512/my-image
+
